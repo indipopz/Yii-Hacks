979453277afee0ab7540cafb70ea95efe80e739a
indipopz <nitish.kr.mallick@gmail.com>
2018-04-06 12:18:38 +0530

    Delete csrf.php

--- .gitignore/csrf.php
+++
@@ -1,5 +0,0 @@
-<form method="POST">
-    <input id="form-token" type="hidden" name="<?=Yii::$app->request->csrfParam?>"
-           value="<?=Yii::$app->request->csrfToken?>"/>
-    <input type="submit" value="ok!">
-</form>
